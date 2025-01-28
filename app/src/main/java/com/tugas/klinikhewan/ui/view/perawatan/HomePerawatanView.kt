ed0538c2f6c629b16ff670d31d32806f1c990341
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-01-28 16:33:07 +0700

    memperbarui tampilan untuk home dari perawatan

---
+++ app/src/main/java/com/tugas/klinikhewan/ui/view/perawatan/HomePerawatanView.kt
@@ -0,0 +1,407 @@
+package com.tugas.klinikhewan.ui.view.perawatan
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tugas.klinikhewan.R
+import com.tugas.klinikhewan.model.Perawatan
+import com.tugas.klinikhewan.ui.CustomWidget.CustomedTopAppBar
+import com.tugas.klinikhewan.ui.navigation.HomePerawatan
+import com.tugas.klinikhewan.ui.viewmodel.PenyediaViewModel
+import com.tugas.klinikhewan.ui.viewmodel.perawatan.HomePerawatanViewModel
+import com.tugas.klinikhewan.ui.viewmodel.perawatan.HomePrwtnUiState
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePerawatnScreen(
+    navigateToAdd: () -> Unit,
+    navigateToBack: () -> Unit,
+    onDetailClickPerawatan: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomePerawatanViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomedTopAppBar(
+                title = HomePerawatan.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getPrwtn()
+                }
+            )
+        },
+        bottomBar = {
+            CustomBottomBarPrwtn(
+                navigateToAdd = navigateToAdd,
+                navigateToBack = navigateToBack,
+            )
+        },
+    ) { innerPadding ->
+
+        HomePerawatanStatus(
+            homePrwtnUiState = viewModel.prwtnUiState,
+            retryAction = { viewModel.getPrwtn() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClickPerawatan,
+            onDeleteClick = {
+                viewModel.deletePrwtn(it.idperawatan)
+                viewModel.getPrwtn()
+            }
+        )
+    }
+}
+
+@Composable
+fun CustomBottomBarPrwtn(
+    navigateToAdd: () -> Unit,
+    navigateToBack: () -> Unit,
+) {
+    BottomAppBar(
+        containerColor = Color(0xFF7C444F),
+        contentColor = Color.White,
+        tonalElevation = 8.dp,
+        modifier = Modifier
+            .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            IconButtonWithRoundedBackground(
+                iconRes = R.drawable.baseline_add_box_24,
+                onClick = navigateToAdd
+            )
+            IconButtonWithRoundedBackground(
+                iconRes = R.drawable.sharp_arrow_back_ios_new_24,
+                onClick = navigateToBack
+            )
+        }
+    }
+}
+
+
+@Composable
+fun IconButtonWithRoundedBackground(
+    iconRes: Int,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .size(64.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(
+                color = Color(0xFFFFF0F0),
+            )
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            painter = painterResource(id = iconRes),
+            contentDescription = null,
+            modifier = Modifier.size(36.dp),
+            tint = Color(0xFFE78895)
+        )
+    }
+}
+
+@Composable
+fun HomePerawatanStatus(
+    homePrwtnUiState: HomePrwtnUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Perawatan) -> Unit = {},
+) {
+    when (homePrwtnUiState) {
+        is HomePrwtnUiState.Loading -> OnPrwtnLoading(modifier = modifier.fillMaxSize())
+
+        is HomePrwtnUiState.Success ->
+            if (homePrwtnUiState.perawatan.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data pasien")
+                }
+            } else {
+                PerawatanLayout(
+                    perawatan = homePrwtnUiState.perawatan,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.idperawatan)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomePrwtnUiState.Error -> OnPrwtnError(
+            retryAction,
+            modifier = modifier.fillMaxSize()
+        )
+    }
+}
+
+
+@Composable
+fun OnPrwtnLoading(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logokopi),
+            contentDescription = stringResource(R.string.loading),
+            modifier = Modifier.size(150.dp)
+        )
+    }
+}
+
+@Composable
+fun OnPrwtnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.nonetwork),
+            contentDescription = "No network"
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center
+        )
+        Button(
+            onClick = retryAction
+        ) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun PerawatanLayout(
+    perawatan: List<Perawatan>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Perawatan) -> Unit,
+    onDeleteClick: (Perawatan) -> Unit = {},
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(perawatan) { kontak ->
+            PerawatanCard(
+                perawatan = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(kontak) },
+                onDeleteClick = {
+                    onDeleteClick(kontak)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun PerawatanCard(
+    perawatan: Perawatan,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Perawatan) -> Unit = {}
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+    Card(
+        modifier = modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFFFF0F0),
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(14.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = perawatan.idperawatan,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = Color(0xFF7C444F),
+                    fontWeight = FontWeight.Bold
+                )
+                IconButton(onClick = { deleteConfirmationRequired = true }) {
+                    Box(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .clip(CircleShape)
+                            .background(Color(0xFFFFD1D1)),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Delete",
+                            tint = Color(0xFF7C444F),
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                }
+            }
+            Divider(
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+                thickness = 1.dp
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_meeting_room_24),
+                    contentDescription = null,
+                    tint = Color(0xFF7C444F),
+                    modifier = Modifier.size(18.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = perawatan.tanggalperawatan,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_text_snippet_24),
+                    contentDescription = null,
+                    tint = Color(0xFF7C444F),
+                    modifier = Modifier.size(18.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = perawatan.detailperawatan,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+    }
+
+    if (deleteConfirmationRequired) {
+        DeleteConfirmationDialogPrwtn(
+            onDeleteConfirm = {
+                deleteConfirmationRequired = false
+                onDeleteClick(perawatan)
+            },
+            onDeleteCancel = {
+                deleteConfirmationRequired = false
+            },
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+
+@Composable
+private fun DeleteConfirmationDialogPrwtn(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = onDeleteCancel,
+        title = {
+            Text(
+                "Hapus Data",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = MaterialTheme.colorScheme.error
+            )
+        },
+        text = {
+            Text(
+                "Apakah Anda yakin ingin menghapus data? Pastikan data yang dihapus benar.",
+                fontSize = 16.sp
+            )
+        },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text("Batal", color = MaterialTheme.colorScheme.onSurfaceVariant)
+            }
+        },
+        confirmButton = {
+            Button(
+                onClick = onDeleteConfirm,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.error
+                )
+            ) {
+                Text("Hapus", color = Color.White)
+            }
+        }
+    )
+}
